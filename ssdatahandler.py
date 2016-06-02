294e70c79902042eadb8122deb1dd44f62d55d78
Ricardo Corral Corral <ricardo@suggestic.com>
2016-06-01 22:49:19 -0500

    load seqVectorizer from scratch

--- ssdatahandler.py
+++ ssdatahandler.py
@@ -1,26 +1,29 @@
-import seqVectorizer as sv
-from sklearn import preprocessing
+import numpy as np
 from keras.utils.np_utils import to_categorical
+from sklearn import preprocessing
+
+import seqVectorizer as sv
+
 
 def create_seq_vectorizer(trainfn='allCATH3_seqVec4_1.txt'):
     seqs = []
     for l in open(trainfn, 'r'):
         l = l.strip().split(',')
         seqs.append(l[3].strip())
-    print seqs
-    _seqVectorizer = sv.loader(comblength=3, verbose=True)
-    #_seqVectorizer.fit_transform(seqs)
+    #print seqs
+    _seqVectorizer = sv.seqVectorizer()#loader(comblength=2, verbose=True)
+    _seqVectorizer.fit_transform(seqs)
     c = ['VFKDGGFTSNDRSVRRYAIRKVLRQMDLGAELGAKTLVLWGGREGAEYDSAKDVSAALDRYREALNL']
     print _seqVectorizer.transform(c)
     return _seqVectorizer
 
 
-def load_dataset(trainfn='allCATH3_seqVec4_1.txt',min_length=10):
+def load_dataset(trainfn='allCATH3_seqVec4_1.txt', min_length=10):
     X, Y = [], []
     for l in open(trainfn, 'r'):
         l = l.strip().split(',')
         _sv = map(float, l[4:])
-        if len(_sv) < min_length:continue
+        if len(_sv) < min_length: continue
         X.append(_sv)
         Y.append(l[2])
 
@@ -30,5 +33,5 @@ def load_dataset(trainfn='allCATH3_seqVec4_1.txt',min_length=10):
 
     return {'X': np.asarray(X),
             'Y': np.asarray(Y),
-            'Y_categorical':to_categorical(Y),
-            'le':le}
+            'Y_categorical': to_categorical(Y),
+            'le': le}
